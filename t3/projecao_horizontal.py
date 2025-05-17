65edb2319c22d3cfd0d14c11ac1c149819ee0ac0
PedroBrasil111 <p260637@dac.unicamp.br>
2025-05-17 17:20:45 -0300

    projecao horizontal t3

--- t3/projecao_horizontal.py
+++ t3/projecao_horizontal.py
@@ -2,36 +2,68 @@ import cv2 as cv
 import numpy as np
 from helper_functions import *
 
-def funcao_objetivo(perfil):
-    func_obj = np.sum(np.square(np.diff(perfil)))
-    return func_obj
+def profile(img):
+    return np.sum(255 - img, axis=1)
 
-def calcular_inclinacao(img, ang1, ang2, passo):
-    melhor_angulo = 0
-    melhor_func_obj = None
-    # Calcula a inclinação da imagem
-    for angulo in range(ang1, ang2, passo):
+def rotate_image(image, angle):
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv.INTER_LINEAR, borderValue=255)    
+    return result
+
+def objective_function(perfil):
+    return np.sqrt(np.sum(np.square(np.diff(perfil))))
+
+def horizontal_projection_inclination(img, ang1, ang2, step):
+    best_angle = 0
+    best_objective_val = None
+
+    for angle in range(ang1, ang2, step):
         # Rotaciona a imagem
-        img_rotacionada = cv.rotate(img, angulo)
+        rotated = rotate_image(img, angle)
+        rotated_profile = profile(rotated)
+        objective_val = objective_function(rotated_profile)
 
-        # Calcula a projeção horizontal
-        perfil = np.sum(img_rotacionada, axis=0)
+        if best_objective_val == None or objective_val > best_objective_val:
+            best_objective_val = objective_val
+            best_angle = angle
 
-        # Calcula a função objetivo
-        func_obj = funcao_objetivo(perfil)
+    return best_angle, best_objective_val
 
-        if melhor_func_obj == None or func_obj > melhor_func_obj:
-            melhor_func_obj = func_obj
-            melhor_angulo = angulo
+def add_histogram(img):
+    profile = np.sum(255 - img, axis=1)
+    min_val, max_val = np.min(profile), np.max(profile)
 
-    return melhor_angulo, melhor_func_obj
+    # 20% da largura da imagem
+    n = np.floor(img.shape[1] * 0.2).astype(int)
+
+    # Cria o histograma
+    profile_img = np.full((img.shape[0], n), 255, dtype=np.uint8)
+    for i in range(img.shape[0]):
+        idx = int(np.floor((profile[i] - min_val) / (max_val - min_val) * (n - 1)))
+        profile_img[i, :idx] = 0
+
+    # Adiciona o histograma na imagem original
+    reshaped = np.zeros((img.shape[0], img.shape[1] + n), dtype=np.uint8)
+    reshaped[:, :img.shape[1]] = img
+    reshaped[:, img.shape[1]:] = profile_img
+
+    return reshaped
 
 def main():
-    img = imread_auto("images/neg_4.png")
-    ang = calcular_inclinacao(img, 0, 360, 1)
+    img = cv.imread("images/pos_24.png", cv.IMREAD_GRAYSCALE)
+
+    print(img.shape)
+    ang, obj = horizontal_projection_inclination(img, -90, 90, 1)
+
+    rotacionada = rotate_image(img, ang)
     # Exibe as imgs
     cv.imshow("Imagem Original", img)
-    cv.imshow("Imagem Rotacionada", cv.rotate(img, ang[0]))
+    cv.imshow("Imagem Rotacionada", rotacionada)
+    cv.imshow("Perfil Original", add_histogram(img))
+    cv.imshow("Perfil", add_histogram(rotacionada))
+    print(f"Melhor ângulo: {ang}°")
+    print(f"Melhor função objetivo: {obj}")
     cv.waitKey(0)
 
 if __name__ == "__main__":
